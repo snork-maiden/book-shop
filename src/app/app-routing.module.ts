8d086b50652c4a329357a5aeeab558288d0da7da
snork-maiden <pollykoldysheva@gmail.com>
2023-11-04 17:31:54 +0300

    Add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { CartComponent } from './cart/cart.component';
+import { MainComponent } from './main/main.component';
+import { BookPageComponent } from './book-page/book-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BaseLayoutComponent,
+    children: [
+      {
+        path: 'cart',
+        component: CartComponent,
+      },
+      {
+        path: '',
+        component: MainComponent,
+      },
+      // {
+      //   path: '',
+      //   component: BookPageComponent,
+      // },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { BooksListComponent } from './books-list/books-list.component';
+import { BookPageComponent } from './book-page/book-page.component';
+import { MainComponent } from './main/main.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseLayoutComponent,
+    BooksListComponent,
+    BookPageComponent,
+    MainComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
