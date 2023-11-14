c3b0a85eb8a8230c98a27b337febf4afa10491a6
snork-maiden <pollykoldysheva@gmail.com>
2023-11-14 11:50:06 +0300

    Bug fix

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -6,7 +6,6 @@ import { getBooksList } from '../services/book-api.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
   cart: Array<BookData> = [];
